04ae88253a322d2b14a1c8521878a1870493fb44
Korewa_Li <li.hvktqs@gmail.com>
2023-11-24 15:25:47 +0700

    Init

---
+++ Util.py
@@ -0,0 +1,67 @@
+import json
+import os
+import sys
+import zipfile
+
+
+def get_current_directory():
+    list = sys.path[0].split(os.sep)
+    return_str = ''
+    for element in list:
+        return_str += element + os.sep
+    return return_str.rstrip(os.sep)
+
+
+# function to add to JSON
+def write_json(filepath, new_data):
+    with open(filepath, 'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data
+        file_data.update(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent=4)
+
+
+# function to extract archive with relevant path and name pattern
+def extractArchive(relevant_path, name_pattern, extract_path):
+    file_name = findFileName(relevant_path, name_pattern)
+
+    with MyZipFile(os.path.join(relevant_path, file_name)) as z:
+        z.extractall(extract_path)
+
+
+# function to find file via pattern
+def findFileName(relevant_path, name_pattern):
+    return [fn for fn in os.listdir(relevant_path)
+            if any(fn.startswith(ext) for ext in name_pattern)][0]
+
+
+class MyZipFile(zipfile.ZipFile):
+
+    def extract(self, member, path=None, pwd=None):
+        if not isinstance(member, zipfile.ZipInfo):
+            member = self.getinfo(member)
+
+        if path is None:
+            path = os.getcwd()
+
+        ret_val = self._extract_member(member, path, pwd)
+        attr = member.external_attr >> 16
+        if attr != 0:
+            os.chmod(ret_val, attr)
+        return ret_val
+
+    def extractall(self, path=None, members=None, pwd=None):
+        if members is None:
+            members = self.namelist()
+
+        if path is None:
+            path = os.getcwd()
+        else:
+            path = os.fspath(path)
+
+        for zipinfo in members:
+            self.extract(zipinfo, path, pwd)
---
+++ main.py
@@ -0,0 +1,84 @@
+import glob
+import json
+import os
+import shutil
+
+import requests
+
+import Util
+
+# Init
+working_path = Util.get_current_directory()
+cache_path = os.path.join(working_path, 'build', 'downloadCache')
+output_path = os.path.join(working_path, 'build', 'output')
+template_path = os.path.join(working_path, 'template')
+
+for folder in [cache_path, output_path, template_path]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+# Clean cache folder
+for cleaningDir in [cache_path, output_path]:
+    for item in os.listdir(cleaningDir):
+        path = os.path.join(cleaningDir, item)
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
+
+# Download installer artifact
+URL = 'https://nightly.link/CleanroomMC/Cleanroom/workflows/BuildTest/dev%2Fmixin3/installer.zip'
+response = requests.get(URL)
+open(os.path.join(cache_path, 'installer.zip'), 'wb').write(response.content)
+
+# Prepare installer and template
+Util.extractArchive(cache_path, 'installer', cache_path)
+Util.extractArchive(cache_path, 'cleanroom', os.path.join(cache_path, 'installer'))
+Util.extractArchive(template_path, 'template', output_path)
+
+# Read cleanroom version
+cleanroom_version = Util.findFileName(cache_path, 'cleanroom').split('-')[1]
+
+# Create libraries folder and copy required
+os.mkdir(os.path.join(output_path, 'libraries'))
+shutil.copyfile(
+    glob.glob(os.path.join(cache_path, 'installer', '**', 'cleanroom*.jar'), recursive=True)[0],
+    os.path.join(output_path, 'libraries', 'cleanroom-{version}-universal.jar'.format(version=cleanroom_version)))
+
+# Create patches file for Cleanroom
+cleanroom_patches_output_path = os.path.join(output_path, 'patches', 'com.cleanroommc.json')
+installer_patches_path = os.path.join(cache_path, 'installer', 'version.json')
+
+with (open(installer_patches_path, 'r') as __in,
+      open(cleanroom_patches_output_path, 'r') as __out):
+    data = json.load(__in)['libraries']
+    out_json = json.load(__out)
+
+    for kd in data:
+        sub_kd = {'name': kd['name']}
+        if 'com.cleanroommc:cleanroom' not in kd['name']:
+            sub_kd.update({'downloads': {'artifact': {
+                'sha1': kd['downloads']['artifact']['sha1'],
+                'size': kd['downloads']['artifact']['size'],
+                'url': kd['downloads']['artifact']['url']
+            }}})
+        else:
+            sub_kd['name'] += '-universal'
+            sub_kd['MMC-hint'] = 'local'
+        out_json['libraries'].append(sub_kd)
+
+    out_json['version'] = cleanroom_version
+with open(cleanroom_patches_output_path, "w") as __out:
+    json.dump(out_json, __out, indent=4)
+
+# Patch mmc-pack.json
+mmc_pack_path = os.path.join(output_path, 'mmc-pack.json')
+
+with open(mmc_pack_path) as mmc_pack:
+    data = json.load(mmc_pack)
+    for item in data['components']:
+        if 'Cleanroom' in item['cachedName']:
+            item['version'] = cleanroom_version
+
+with open(mmc_pack_path, 'w') as __out:
+    json.dump(data, __out, indent=4)
